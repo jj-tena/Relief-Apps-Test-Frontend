ab71aa1ceccd2156c5dae1e1679223d432236adc
josejustotena0@gmail.com <josejustotena0@gmail.com>
2021-10-03 16:24:30 +0200

    Create the four basic components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { VideoViewComponent } from './video-view/video-view.component';
+import { HistoryComponent } from './history/history.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    VideoViewComponent,
+    HistoryComponent,
+    BookmarksComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/video-view/video-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'videoView',
+  templateUrl: './video-view.component.html',
+  styleUrls: ['./video-view.component.css']
+})
+export class VideoViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
